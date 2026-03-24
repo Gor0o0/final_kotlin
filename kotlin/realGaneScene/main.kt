dad2a009ce8f2212d2b3ade3db111a23b19586ef
Goro <moasobakaposholvon@gmail.com>
2026-03-24 11:19:02 +0300

    Add files via upload

---
+++ kotlin/realGaneScene/main.kt
@@ -0,0 +1,187 @@
+package realGaneScene
+
+import de.fabmax.kool.KoolApplication           // KoolApplication - запускает Kool-приложение (окно + цикл рендера)
+import de.fabmax.kool.addScene                  // addScene - функция "добавь сцену" в приложение (у тебя она просила отдельный импорт)
+import de.fabmax.kool.math.Vec3f                // Vec3f - 3D-вектор (x, y, z), как координаты / направление
+import de.fabmax.kool.math.deg                  // deg - превращает число в "градусы" (угол)
+import de.fabmax.kool.modules.audio.synth.SampleNode
+import de.fabmax.kool.scene.*                   // scene.* - Scene, defaultOrbitCamera, addColorMesh, lighting и т.д.
+import de.fabmax.kool.modules.ksl.KslPbrShader  // KslPbrShader - готовый PBR-шейдер (материал)
+import de.fabmax.kool.util.Color                // Color - цвет (RGBA)
+import de.fabmax.kool.util.Time                 // Time.deltaT - сколько секунд прошло между кадрами
+import de.fabmax.kool.pipeline.ClearColorLoad   // ClearColorLoad - режим: "не очищай экран, оставь то что уже нарисовано"
+import de.fabmax.kool.modules.ui2.*             // UI2: addPanelSurface, Column, Row, Button, Text, dp, remember, mutableStateOf
+
+// Flow корутины
+import kotlinx.coroutines.launch                    // запуск корутин
+import kotlinx.coroutines.flow.MutableSharedFlow    // радиостанция событий
+import kotlinx.coroutines.flow.SharedFlow           // чтение для подписчиков
+import kotlinx.coroutines.flow.MutableStateFlow     // табло состояний
+import kotlinx.coroutines.flow.StateFlow            // только для чтения
+import kotlinx.coroutines.flow.asSharedFlow         // отдать наружу только SharedFlow
+import kotlinx.coroutines.flow.asStateFlow          // отдать только StateFlow
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.flatMapLatest
+
+enum class QuestState {
+    START,
+    WAIT_HERB,
+    GOOD_END,
+    EVIL_END
+}
+
+// -= Типы объектов игрового мира
+enum class WorldObjectType{
+    ALCHEMIST,
+    HERB_SOURCE
+}
+
+// -= Описание объектов в игрровом мире
+data class WorldObjectDef(
+    val id: String,
+    val type: WorldObjectType,
+    val x: Float,
+    val z: Float,
+    val interactRadius: Float
+)
+
+data class NpcMemory(
+    val hasMet: Boolean, // -= встретил или нет
+    val timesTalked: Int, // -= Сколько раз уже поговорил
+    val receivedHerb: Boolean
+)
+
+data class PlayerState(
+    val playerId: String,
+    val posX: Float,
+    val posZ: Float,
+    val questState: QuestState,
+    val inventory: Map<String, Int>, // -= примитивный словарь
+    val alchemistMemory: NpcMemory,
+    val currentAreaId: String?,
+    val hintText: String, // -= Подсказка что делать и тп
+)
+
+// -=-=-= Вспомогательные функции =-=-=-
+fun herbCount(player: PlayerState): Int{ //> даёт количество herb
+    return player.inventory["herb"] ?: 0
+}
+
+fun distance2d(ax: Float, az: Float, bx: Float, bz: Float): Float{
+    // sqrt((dx * dx) + (dz * dz))
+    val dx = ax - bx
+    val dz = az - bz
+    return kotlin.math.sqrt(dx*dx + dz*dz)
+}
+
+fun initialPlayerState(playerId: String): PlayerState {
+    return if(playerId == "Stas"){
+        PlayerState(
+            "Stas",
+            0f,
+            0f,
+            QuestState.START,
+            emptyMap(),
+            NpcMemory(
+                true,
+                2,
+                false
+            ),
+            null,
+            "Подойди к одной из локаций"
+        )
+    }else{
+        PlayerState(
+            "Oleg",
+            0f,
+            0f,
+            QuestState.START,
+            emptyMap(),
+            NpcMemory(
+                false,
+                0,
+                false
+            ),
+            null,
+            "Подойди к одной из локаций"
+        )
+    }
+}
+
+data class DialogueOption(
+    val id: String,
+    val text: String
+)
+
+data class DialogueView(
+    val npcId: String,
+    val text: String,
+    val options: List<DialogueOption>
+)
+
+fun buildAlchemistDialogue(player: PlayerState): DialogueView{
+    val herbs = herbCount(player)
+    val memory = player.alchemistMemory
+
+    return when(player.questState){
+        QuestState.START -> {
+            val greeting =
+                if (!memory.hasMet) {
+                    "Привет, ты кто"
+                }else{
+                    "Ну что ${player.playerId} я жду?!"
+                }
+            DialogueView(
+                "Алхимик",
+                "$greeting\nТащи траву",
+                listOf(
+                    DialogueOption("accept_help", "Акей"),
+                    DialogueOption("threat", "Нит, ты давай")
+                )
+            )
+        }
+        QuestState.WAIT_HERB -> {
+            if (herbs < 3) {
+                DialogueView(
+                    "Алхимик",
+                    "Мало, мне надо 4 вщто",
+                    emptyList<>()
+                )
+            }else{
+                DialogueView(
+                    "Алхимик",
+                    "спс",
+                    listOf(
+                        DialogueOption("give_herb", "Отдать 4 травы")б
+                    )
+                )
+            }
+        }
+        QuestState.GOOD_END -> {
+            val text =
+                if (memory.receivedHerb){
+                    "Ну что, похимичим?!"
+                }else{
+                    "Ты завершил квест, но память не обновилась, капут"
+                }
+            DialogueView(
+                "Алхимик",
+                text,
+                emptyList()
+            )
+        }
+        QuestState.EVIL_END -> {
+            DialogueView(
+                "Алхимик",
+                "я с тобой больше не дружу",
+                emptyList()
+            )
+        }
+    }
+}
